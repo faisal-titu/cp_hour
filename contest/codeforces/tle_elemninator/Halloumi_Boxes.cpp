73f0f0bfb905f3516cddcad8d15da5ee07a98fdb
faisal-titu <codeisfun99@gmail.com>
2025-02-18 10:19:31 +0600

    new begining

---
+++ contest/codeforces/tle_elemninator/Halloumi_Boxes.cpp
@@ -0,0 +1,16 @@
+// author: faisal ahammed
+// date: 10:17:45 18-02-2025
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void accepted()
+{
+    
+}
+
+int main()
+{
+    accepted();
+    return 0;
+}
---
+++ leetcode/9_Palindrome_Number.cpp
@@ -0,0 +1,16 @@
+// author: faisal ahammed
+// date: 13:34:19 18-11-2024
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void accepted()
+{
+    
+}
+
+int main()
+{
+    accepted();
+    return 0;
+}
