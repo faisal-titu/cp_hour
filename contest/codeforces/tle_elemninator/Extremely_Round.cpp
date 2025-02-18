1b04cdcdff9b4d44d9945cfd1f177c65fc0e40fe
faisal-titu <codeisfun99@gmail.com>
2025-02-18 23:33:34 +0600

    sovled 2

---
+++ contest/codeforces/tle_elemninator/Extremely_Round.cpp
@@ -0,0 +1,35 @@
+// author: faisal ahammed
+// date: 22:48:51 18-02-2025
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void accepted()
+{
+    int t;cin >>t;
+    while(t--)
+    {
+        string s;
+        cin >> s;
+        int len = s.size();
+
+        string multiplier = "1";
+        for(int i=0;i<len-1;i++)  multiplier += '0';
+        
+
+        int num = stoi(s);
+        int mul = stoi(multiplier);
+
+        
+
+
+
+
+    }
+}
+
+int main()
+{
+    accepted();
+    return 0;
+}
--- contest/codeforces/tle_elemninator/Halloumi_Boxes.cpp
+++ contest/codeforces/tle_elemninator/Halloumi_Boxes.cpp
@@ -6,7 +6,26 @@ using namespace std;
 
 void accepted()
 {
-    
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n, k;
+        cin >> n >> k;
+        vector<int> a(n);
+        for(int i = 0; i < n; i++) cin >> a[i];
+        
+        vector<int> temp = a;
+        sort(temp.begin(), temp.end());
+
+        if(temp == a)
+        {
+            cout << "YES\n";
+            continue;;
+        }
+        if(k == 1) cout << "NO\n";
+        else cout << "YES\n"; 
+    }
 }
 
 int main()
